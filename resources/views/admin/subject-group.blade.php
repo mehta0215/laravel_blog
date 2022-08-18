57a1681aecccccc96a1554362b08888d855fd0f7
Abhinav Mehta2 <abhinavm2@chetu.com>
2022-08-18 17:28:53 +0530

    Edit Commit

--- resources/views/admin/subject-group.blade.php
+++ resources/views/admin/subject-group.blade.php
@@ -191,7 +191,7 @@
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title">Update Subject</h4>
+                                <h4 class="modal-title">Update Subject Group</h4>
                             </div>
                         <div class="modal-body">
                            <form role="form" method="POST" action="">
